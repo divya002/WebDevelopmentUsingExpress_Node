30daa34b8179486a2b2452a58d326f2bc9d91017
divya002 <divyaprakash002@gmail.com>
2017-08-03 23:47:29 +0530

    SettingUpExpress

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require('express');
+
+var app=express();
+var port=5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.listen(port,function(err){
+    console.log("Running server on port "+port);
+    });
+app.get("/",function(req,res){
+res.send("I am Root of file");
+});
