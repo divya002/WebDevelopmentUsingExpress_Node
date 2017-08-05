c3ffebfb4382b6ff33f2ad867297bcfc1778ef93
divya002 <divyaprakash002@gmail.com>
2017-08-05 17:02:15 +0530

    gulp nodemon,gulp-inject added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express=require('express');
+var express = require('express');
 
-var app=express();
-var port=5000;
+var app = express();
+var port = process.env.port || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 app.listen(port,function(err){
-    console.log('Running server on port '+port);
+        console.log('Running server on port ' + port);
     });
-app.get('/',function(req,res){
-res.send('I am Root of file');
+app.get('/root',function(req,res) {
+    res.send('I am Root of file');
 });
