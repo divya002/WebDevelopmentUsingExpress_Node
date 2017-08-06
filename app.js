2318ecab6bc7c07f4cdaaf174a6c0e7b57ebefb7
divya002 <divyaprakash002@gmail.com>
2017-08-06 10:39:10 +0530

    Navigation working

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 5000;
 
-//app.use(express.static('public'));
-//app.use(express.static('src/views'));
+app.use(express.static('public'));
+//app.use(express.static('src/views'));//commenting
 app.set('views','src/views');
-app.set('view engine','jade');
+app.set('view engine','ejs');
 
 app.listen(port,function(err){
         console.log('Running server on port ' + port);
     });
-app.get('/',function(req,res){
-    res.render('index');
-})
+app.get('/',function(req,res) {
+    res.render('index',{
+        title:'hello from render',
+        nav:[{
+            Link:'/Books',
+            Text:'Books'
+        },{
+            Link:'/Authors',
+            Text:'Authors'
+        }]
+    });
+});
+
 app.get('/root',function(req,res) {
     res.send('I am Root of file');
 });
